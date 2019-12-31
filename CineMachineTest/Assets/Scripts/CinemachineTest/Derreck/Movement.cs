1d3e681d0f5dfd7258e38177fee5d4ab3a86720f
Derreck O'Neil <derreck_oneil@bloomfield.edu>
2019-12-30 20:38:19 -0500

    More tests done with scripts

---
+++ CineMachineTest/Assets/Scripts/CinemachineTest/Derreck/Movement.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace CinemachineTest.Derreck
+{
+    public class Movement : MonoBehaviour
+    {
+        [Range(-1,10)]
+        [SerializeField] private float speedValue = 5;
+        public void Update()
+        {
+            transform.Translate(Vector3.up * Time.deltaTime * speedValue);
+        }
+    }
+}
---
+++ CineMachineTest/Assets/Scripts/CinemachineTest/Derreck/VCManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using Cinemachine;
+
+
+namespace CinemachineTest.Derreck
+{
+    
+    public class VCManager : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject camera1;
+        [SerializeField]
+        private GameObject camera2;
+        [SerializeField]
+        private Camera cam;
+        Movement MovementScript;
+        [SerializeField]
+        private GameObject cube;
+        YieldInstruction waitInterval = new WaitForSeconds(5);
+        public void Update()
+        {
+
+            StartCoroutine(Camera());
+        }
+        public void Awake()
+        {
+            MovementScript = cube.GetComponent<Movement>();
+            MovementScript.enabled = false;
+        }
+        private IEnumerator Camera()
+        {
+            
+            yield return waitInterval;
+            Debug.Log("Changing Priority!");
+            camera2.GetComponent<CinemachineVirtualCamera>().Priority = 1;
+            MovementScript.enabled = true;
+            yield return waitInterval;
+
+        }
+    }
+}
