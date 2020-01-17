6786467e14225ae99be454ed8f1be1a89fa44ce6
Derreck O'Neil <derreck_oneil@bloomfield.edu>
2020-01-16 20:26:38 -0500

    more changes

---
+++ CineMachineTest/Assets/Scripts/CinemachineTest/Derreck/SphereMovement.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace CinemachineTest.Derreck
+{
+    public class SphereMovement : MonoBehaviour
+    {
+        [SerializeField]
+        private AnimationCurve xCurve;
+        public void Update()
+        {
+            //Vector3 newPos = new Vector3(xCurve.Evaluate(0), xCurve.Evaluate(0), 0);
+            //transform.position = newPos;
+        }
+    }
+}
